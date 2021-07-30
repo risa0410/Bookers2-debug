577ada093d3b1e0025f0afd8dfdcff6525783ef1
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-07-30 11:21:39 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   def create
     @user = current_user
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
